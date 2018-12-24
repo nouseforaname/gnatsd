28ef5f95a6507aa5c6bfea1c3599db2f9e72ef74
Rebecca Putinski <rputinski@pivotal.io>
2018-12-24 11:22:40 -0500

    Lock go-nats client to run 1.3.0 gnatsd tests
    - Upstream gnatsd@master already makes the necessary changes to handle new go-nats changes, though unreleased for us to consume
    
    [#162818815](https://www.pivotaltracker.com/story/show/162818815)

--- ci/tasks/test.sh
+++ ci/tasks/test.sh
@@ -12,4 +12,12 @@ cd $GOPATH/src/github.com/nats-io/gnatsd
 service rsyslog start
 
 go get -t ./...
+
+# Workaround since the latest go-nats client doesn't work with tests for 1.3.0
+# RP: https://github.com/nats-io/gnatsd/issues/866
+# We should really be testing yagnats against these
+pushd ../go-nats
+  git checkout -q 21aecb5f7f78c45d4aed681a53d45fe9681ad537
+popd
+
 go test ./...
