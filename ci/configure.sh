d6d6c1935a05a913fa03f6d7bc63db8371b2588d
James Myers <jmyers@pivotal.io>
2019-06-10 14:28:30 -0700

    Move pipeline to the bosh-director team
    [#166391315](https://www.pivotaltracker.com/story/show/166391315)
    
    Co-authored-by: Belinda Liu <bliu@pivotal.io>

--- ci/configure.sh
+++ ci/configure.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-exec fly -t production set-pipeline \
+exec fly -t director set-pipeline \
   -p gnatsd \
   -c ./pipeline.yml \
   --load-vars-from <(lpass show --note "bosh gnatsd concourse secrets")
